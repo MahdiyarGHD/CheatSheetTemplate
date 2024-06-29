9ed907ac410b26b57ad305dd72bca43831b93e3b
MahdiyarGHD <m.gh.1385.mahdiyar@gmail.com>
2024-06-29 19:46:20 +0330

    feat: Added top section of home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import Header from "../components/Header";
+import SearchInput from "../components/SearchInput";
+import '../assets/fonts/Icomoon/style.css';
 
 const Home = () => {
     return <>
         <Header />
+        <div dir="rtl" className="w-full mt-10 text-white/70 font-bold text-2xl text-center">
+            برگه‌تقلب‌ Tailwind
+        </div>
+        <SearchInput />
     </>
 }
 
